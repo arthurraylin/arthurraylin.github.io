a64dd2804d47ca6789fac57bb70f1874429cb4d4
ArthurRay <arthurraylin@ArthurRays-Air.fios-router.home>
2017-10-10 18:58:05 -0400

    js1

---
+++ Lab/js1/assets/js/app.js
@@ -0,0 +1,30 @@
+
+
+var cash = 100000;
+// global charge value
+
+update_wallet();
+
+$('.item').click(function() {
+	var item = $(this).data('item'),
+	    amount = $(this).data('amount');
+
+	charge_card(amount, item);
+});
+
+
+function charge_card(amount, item) {
+
+	if (cash >= amount) {
+		cash = cash - amount;
+		update_wallet();
+		$('.closet-items').append(item + ', ');
+		// console.log(cash);
+	} else {
+		alert('not enough money.')
+	}
+}
+
+function update_wallet() {
+	$('.cash').html(cash);
+}
